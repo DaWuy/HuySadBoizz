2b732579cdd78618db197c9282e93087267755f2
DaWuy <uchihahuypro@gmail.com>
2021-12-14 23:48:34 +0700

    HuySadBoiz

--- modules/commands/Bonk.js
+++ modules/commands/Bonk.js
@@ -1,20 +1,21 @@
-﻿const fs = require("fs");
+const fs = require("fs");
 module.exports.config = {
-	name: "chubot2",
+	name: "Bonk",
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "DW", 
 	description: "no prefix",
 	commandCategory: "Không cần dấu lệnh",
-	usages: "chủ bot tên gì",
+	usages: "Bonk",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Chủ bot tên gì")==0 || (event.body.indexOf("chủ bot tên gì")==0 || (event.body.index0f("Tên chủ bot")==0 || (event.body.index0f("tên chủ bot")==0)))) {
+	if (event.body.indexOf("Bonk")==0 || (event.body.indexOf("bonk")==0)) {
 		var msg = {
-				body: "Cậu chủ tên là Trần Nguyễn Gia Huy, hay còn gọi là DaWuy :3",
+				body: "Gõ chết mẹ mày!",
+				attachment: fs.createReadStream(__dirname + `/noprefix/bonk.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
--- modules/commands/chubot.js
+++ modules/commands/chubot.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Chủ bot là ai")==0 || (event.body.indexOf("chủ bot là ai")==0)) {
+	if (event.body.indexOf("Chủ bot là ai")==0 || (event.body.indexOf("chủ bot là ai")==0 || (event.body.inedx0f("ai là chủ bot")==0 || (event.body.index0f("Ai là chủ bot")==0)))) {
 		var msg = {
-				body: "Là cậu chủ Trần Nguyễn Gia Huy",
+				body: "Là cậu chủ Trần Nguyễn Gia Huy, hay còn được gọi là DaWuy đó ạ :3",
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
