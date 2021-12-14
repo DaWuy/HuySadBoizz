6ba28b11d8dc1014f7636307d4872697cb1ab979
DaWuy <uchihahuypro@gmail.com>
2021-12-14 16:27:07 +0700

    HuySadBoiz

--- modules/commands/sinhnhatad.js
+++ modules/commands/sinhnhatad.js
@@ -9,11 +9,11 @@ module.exports.config = {
 }
 
 module.exports.run = function ({ event, api }) {
-    const t = Date.parse("june 16, 2022 00:00:00") - Date.parse(new Date());
+    const t = Date.parse("october 20, 2022 00:00:00") - Date.parse(new Date());
     const seconds = Math.floor( (t/1000) % 60 );
     const minutes = Math.floor( (t/1000/60) % 60 );
     const hours = Math.floor( (t/(1000*60*60)) % 24 );
     const days = Math.floor( t/(1000*60*60*24) );
 
-    return api.sendMessage(`「Sắp tới ngày sinh nhật của admin rồi đóoooooo <3」\n» ${days} ngày ${hours} tiếng ${minutes} phút ${seconds} giây «`, event.threadID, event.messageID);
+    return api.sendMessage(`「Sắp tới ngày sinh nhật của cậu chủ Trần Nguyễn Gia Huy rồi đóooo <3」\n» ${days} ngày ${hours} tiếng ${minutes} phút ${seconds} giây «`, event.threadID, event.messageID);
 }
