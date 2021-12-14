4808643c6233f1f754aacb134e9398c141afa4aa
DaWuy <uchihahuypro@gmail.com>
2021-12-14 19:48:44 +0700

    HuySadBoiz

--- modules/commands/chubot2.js
+++ modules/commands/chubot2.js
@@ -1,18 +1,18 @@
 ﻿const fs = require("fs");
 module.exports.config = {
-	name: "Chủ bot là ai",
+	name: "chubot2",
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "DW", 
 	description: "no prefix",
 	commandCategory: "Không cần dấu lệnh",
-	usages: "Chủ bot là ai",
+	usages: "chủ bot tên gì",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Chủ bot tên gì")==0 || (event.body.indexOf("chủ bot tên gì")==0)){
+	if (event.body.indexOf("Chủ bot tên gì")==0 || (event.body.indexOf("chủ bot tên gì")==0)) {
 		var msg = {
 				body: "Là cậu chủ Trần Nguyễn Gia Huy",
 			}
